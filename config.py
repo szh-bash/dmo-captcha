4c7351a79ff8dd7614cbd84656c743a4059c157b
Immortal.S <342333349@qq.com>
2020-08-20 01:13:30 +0800

    clean captch

--- config.py
+++ config.py
@@ -3,16 +3,16 @@ import progressbar as pb
 
 # build data
 captcha_origin_path = '/data/shenzhonghai/dmo-captcha/train-origin'
-captchaPath = '/dev/shm/dmo-captcha-train'
+captchaPath = '/dev/shm/dmo-captcha-part0'
 test_origin_path = '/data/shenzhonghai/dmo-captcha/test-origin'
-testPath = '/dev/shm/dmo-captcha-test'
+testPath = '/dev/shm/dmo-captcha-part1'
 
 # train
 Total = 100
 batch_size = 64
 learning_rate = 0.001
 weight_decay = 0.00000
-modelSavePath = '/data/shenzhonghai/dmo-captcha/models/vgg16_224_256_sort'
+modelSavePath = '/data/shenzhonghai/dmo-captcha/models/vgg16_32_48'
 # test
 modelPath = '/data/shenzhonghai/dmo-captcha/models/vgg16_224_256_sort.tar'
 dp = 0.00
--- train.sh
+++ train.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES="6,0,9,5" python -u train.py | tee /data/shenzhonghai/dmo-captcha/logs/vgg16_224_256_sort.log
+CUDA_VISIBLE_DEVICES="6,0,9,5" python -u train.py | tee /data/shenzhonghai/dmo-captcha/logs/vgg16_32_48.log

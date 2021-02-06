677a4c135983cf80ff51c0e15676a84193152c5b
Immortal.S <342333349@qq.com>
2021-02-06 10:33:48 +0800

    fix worker crash

--- service/recognizer.py
+++ service/recognizer.py
@@ -11,7 +11,8 @@ from model.resnet import resnet50
 from model.effnet import EffNet
 # modelPath = 'C:/DATA/dmo/resnet_36_56_m30_co_clean6.tar'
 # modelPath = 'C:/DATA/dmo/effnet_base.tar'
-modelPath = 'C:/DATA/dmo/effnet_s32_m10.tar'
+# modelPath = 'C:/DATA/dmo/effnet_s32_m10.tar'
+modelPath = '/data/shenzhonghai/dmo-captcha/models/effnet_s32_m10.tar'
 
 
 H = 64
--- service/worker.py
+++ service/worker.py
@@ -2,18 +2,23 @@ import os
 import time
 from recognizer import predict
 
-path = 'D:/Game/DigimonMasters/Code_AI/IShiled/question/'
-# path = /pic
+# path = 'D:/Game/DigimonMasters/Code_AI/IShiled/question/'
+path = '/data/shenzhonghai/dmo-captcha/pic/'
+# path = 'C:/pic/'
 if __name__ == '__main__':
     while True:
         dir = os.listdir(path)
         for ids in dir:
-            if not('_' in ids):
+            if not('_' in ids) and ids[-4:] == '.jpg':
                 timer = time.time()
                 print(path+ids)
-                answer = predict(path+ids)
-                timer = time.time() - timer
-                os.rename(path+ids, path+ids[:-4]+'_'+str(answer)+'.jpg')
-                print('Time: %.2fs' % timer)
-                print('Result: %s' % answer)
-        time.sleep(0.5)
+                try:
+                    answer = predict(path+ids)
+                except:
+                    print('Error: recognization failed!')
+                else:
+                    timer = time.time() - timer
+                    os.rename(path+ids, path+ids[:-4]+'_'+str(answer)+'.jpg')
+                    print('Time: %.2fs' % timer)
+                    print('Result: %s' % answer)
+        time.sleep(1)

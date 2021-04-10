c6e5d7d7d73d8a80d96bcc6d440e002a84d3ed7f
Immortal.S <342333349@qq.com>
2021-04-11 00:28:54 +0800

    standardizing readme

--- service/cfg.py
+++ service/cfg.py
@@ -2,7 +2,7 @@
 # modelPath = 'C:/DATA/dmo/effnet_base.tar'
 # modelPath = 'C:/DATA/dmo/effnet_s32_m10.tar'
 # modelPath = 'C:/DATA/dmo/effnet_lr4045_s32_m10_co_rot_trs_crop_FULL_borWhite_rsz128.tar'
-modelPath = 'C:/DATA/dmo/effnet_lr4045_s32_m30_co_rot_trs_crop_FULL_borWhite_sz64.tar'
+modelPath = 'D:/Game/DigimonMasters/IShield/models/effnet_lr4045_s32_m30_co_rot_trs_crop_FULL_borWhite_sz64.tar'
 # modelPath = '/data/shenzhonghai/dmo-captcha/models/effnet_s32_m10.tar'
 # modelPath = 'AImodel.tar'
 
